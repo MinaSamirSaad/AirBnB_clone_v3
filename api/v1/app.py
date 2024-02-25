1e0472c07bce2b7d5e316ce469a84442e88f4eda
MinaSamirSaad <minasamir.nashid@gmail.com>
2024-02-25 19:37:55 +0200

    task3

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
@@ -5,15 +6,18 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.route('/api/v1/status', methods=['GET'])
 def status():
     return jsonify({"status": "OK"})
+
 
 @app.teardown_appcontext
 def teardown(self):
     storage.close()
 
+
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST','0.0.0.0')
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = getenv('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True)
