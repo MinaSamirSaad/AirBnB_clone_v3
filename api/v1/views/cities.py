3cfdfe8dc86dd3ed0e1fc4620d8b758910dd6681
MinaSamirSaad <minasamir.nashid@gmail.com>
2024-02-27 01:56:02 +0200

    update task 7

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
 """ Initialize the blueprint app_views """
 from api.v1.views import app_views
-from flask import abort, jsonify
+from flask import abort, jsonify, request
+from models import storage
 
 
 @app_views.route('states/<state_id>/cities',
                  methods=['GET'], strict_slashes=False)
 def get_cities(_id):
     """ returns a JSON: {"status": "OK"}"""
-    from models import storage
     cities = storage.all("City")
     list_states = storage.all('State')
     if "State.{}".format(_id) not in list_states:
         abort(404)
-    list = [city.to_dict() for city in cities.values() if city.state_id == _id]
-    return jsonify(list)
+    lis = [city.to_dict() for city in cities.values() if city.state_id == _id]
+    return jsonify(lis)
 
 
 @app_views.route('cities/<city_id>', methods=['GET'], strict_slashes=False)
 def get_city(city_id):
     """ returns a JSON: {"status": "OK"}"""
-    from models import storage
     cities = storage.all("City")
     for city in cities.values():
         if city.id == city_id:
@@ -31,7 +30,6 @@ def get_city(city_id):
 @app_views.route('cities/<city_id>', methods=['DELETE'], strict_slashes=False)
 def delete_city(city_id):
     """ returns a JSON: {"status": "OK"}"""
-    from models import storage
     cities = storage.all("City")
     for city in cities.values():
         if city.id == city_id:
@@ -45,11 +43,9 @@ def delete_city(city_id):
                  methods=['POST'], strict_slashes=False)
 def post_city(state_id):
     """ returns a JSON: {"status": "OK"}"""
-    from models import storage
-    from flask import request
     data = request.get_json()
     if data is None:
-        abort(400, "Not a JSON") 
+        abort(400, "Not a JSON")
     if "name" not in data:
         abort(400, "Missing name")
     from models.city import City
@@ -67,8 +63,6 @@ def post_city(state_id):
 @app_views.route('cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def put_city(city_id):
     """ returns a JSON: {"status": "OK"}"""
-    from models import storage
-    from flask import request
     data = request.get_json()
     if data is None:
         abort(400, "Not a JSON")
