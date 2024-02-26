9bff198b84ea3aa3322794b51cf217601b833bea
mina-william-23 <capeandroid50@gmail.com>
2024-02-26 20:42:57 +0200

    test amenity

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -67,7 +67,7 @@ def put_amenity(amenity_id):
     amenities = storage.all("Amenity")
     for amenity in amenities.values():
         if amenity.id == amenity_id:
-            amenity.name = data['name']
+            # amenity.name = data['name']
             amenity.save()
             return jsonify(amenity.to_dict()), 200
     return jsonify({"error": "Not found"}), 404
